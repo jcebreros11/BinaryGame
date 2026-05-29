d44448a19ba51c282c99bd3a416723218eb1cd68
George Nassour <gnasour@gmail.com>
2026-05-29 13:03:51 -0700

    Implementing different base inputs

--- src/utils/binaryUtils.js
+++ src/utils/binaryUtils.js
@@ -1,17 +1,48 @@
-function binaryUtils(){
-    const getBaseLog = (base, arg)  => {
+function binaryUtils() {
+    const getBaseLog = (base, arg) => {
         return Math.log(arg) / Math.log(base);
     }
 
     const getPerfectWordSize = (byteLength) => {
 
     }
+
+    const formatHexNumber = (hexAnswer) => {
+        
+    }
+
+    const formatOctNumber = (octAnswer) => {
+        
+    }
+
+    const formatAnswer = (answer) => {
+
+        answer = answer.slice();        
+        if (answer[0] === '0') {
+
+            switch (answer[1]) {
+                case 'x':
+                case 'X':
+                    return formatHexNumber(answer.slice(2));
+                    break;
+                
+                default:
+                    return formatOctNumber(answer.slice(1));
+            }
+        } else {
+            return formatBinaryNum(answer);
+        }
+    }
     
-    const  formatBinaryNum = (shortBinaryNumber) => {
+    const formatBinaryNum = (shortBinaryNumber) => {
+        
         let binNumCharArr = [];
         let binNum = parseInt(shortBinaryNumber, 2);
         const sign = shortBinaryNumber >= 0 ? 1 : -1;
     
+        // Edge cases,
+        // since log isnt defined on argument 0
+        // and log(1) is 0, so algorithm incorrectly assignd it 0 bytes
         if (binNum == 0) {
             return "00000000";
         } else if (binNum == 1){
